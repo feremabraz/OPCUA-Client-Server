f9fe100d7d30bd055ddad45d4d3933f60bdb0e55
Fernando <fb@gamekaiju.monster>
2023-07-01 06:26:49 +0100

    Added XML documentation.

--- OPCUA-Client/ClientProgram.cs
+++ OPCUA-Client/ClientProgram.cs
@@ -3,6 +3,10 @@ using Opc.UaFx.Client;
 
 internal static class ClientProgram
 {
+    /// <summary>
+    /// Connects to an OPC server, provides a menu for browsing and retrieving node values,
+    /// and disconnects from the server upon exit.
+    /// </summary>
     private static void Main()
     {
         using var client = new OpcClient("opc.tcp://localhost:4840");
@@ -49,30 +53,51 @@ internal static class ClientProgram
         client.Disconnect();
     }
 
+    /// <summary>
+    /// Retrieves the value of a known OPC UA node identified by its node identifier.
+    /// </summary>
+    /// <param name="client">The OPC UA client.</param>
     private static void GetKnownNodeValue(OpcClient client)
     {
         Console.WriteLine("Enter a OPC UA node identifier for a property.");
         Console.WriteLine("For example: ns=2;s=Production Line 1/SMT Machine 2/ComponentsPerHour");
+
+        // Create an OpcReadNode object with the specified node identifier
         string? nodeId = Console.ReadLine();
         OpcReadNode nodeToRead = new OpcReadNode(nodeId);
+        
+        // Read the value of the node from the OPC UA server and print it down
         OpcValue nodeValue = client.ReadNode(nodeToRead);
         Console.WriteLine("Node: {0}, Value: {1}", nodeId, nodeValue.Value);
     }
-    
+
+    /// <summary>
+    /// Recursively browses the OPC UA server starting from the specified node,
+    /// printing the node information with indentation.
+    /// </summary>
+    /// <param name="node">The starting node to browse.</param>
+    /// <param name="startPrinting">Determines if printing should start for the current node and its children.</param>
+    /// <param name="level">The current level of indentation.</param>
     private static void BrowseServer(OpcNodeInfo node, bool startPrinting = true, int level = 0)
     {
 
+        // Check if printing should start based on the node ID
         if (startPrinting == false && node.NodeId.ToString().Contains("Production Line 1"))
         {
             startPrinting = true;
         }
 
+        // Print node information if printing is allowed for the current node
         if (startPrinting)
         {
+            // Construct the formatted string with indentation, display name, and node ID
+            // This creates a string consisting of a specified character ('.') repeated a certain number of times.
+            // The purpose of this is to add indentation to the string based on the current level. Each level corresponds to a nested node.
             Console.WriteLine("{0}{1}({2})", new string('.', level * 4), node.Attribute(OpcAttribute.DisplayName).Value, node.NodeId);
             startPrinting = true;
         }
 
+        // Recursively browse the child nodes
         level++;
         foreach (var childNode in node.Children())
         {
