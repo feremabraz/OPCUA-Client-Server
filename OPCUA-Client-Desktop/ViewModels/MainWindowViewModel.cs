5bbf8623307541efbbb9b7c991dc9a84c74fb7be
Fernando <fb@gamekaiju.monster>
2023-07-01 03:15:54 +0100

    Fixes bug related to the current design-previewer release candidate.

--- OPCUA-Client-Desktop/ViewModels/MainWindowViewModel.cs
+++ OPCUA-Client-Desktop/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,9 @@ using System.Reactive;
 using ReactiveUI;
 using OPCUA_Client_Desktop.Services;
 
+// Disables warning pertaining to a problem in the Avalonia 11 RC design previewer.
+#pragma warning disable CS8618
+
 namespace OPCUA_Client_Desktop.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
@@ -10,10 +13,15 @@ public class MainWindowViewModel : ViewModelBase
     private IOpcClientService _opcClientService;
     private bool _isConnected;
     private bool _serverError;
-
-    public MainWindowViewModel(IOpcClientService opcClientService)
+    
+    public MainWindowViewModel()
     {
         ConnectCommand = ReactiveCommand.Create(Connect);
+        if (!Avalonia.Controls.Design.IsDesignMode) _opcClientService = new OpcClientService();
+    }
+
+    public MainWindowViewModel(IOpcClientService opcClientService) : this()
+    {
         _opcClientService = opcClientService;
     }
     
